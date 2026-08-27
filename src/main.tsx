5fc4e33eef4341d290697694837db0b36f75f66f
Hanjay6688 <jktr9n2g88@privaterelay.appleid.com>
2026-08-27 21:12:48 +0700

    feat: enable live QC Supabase bridge

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ import './interaction-upgrades'
 import './global-readability.css'
 import './qc-final-sku.css'
 import './qc-final-sku'
+import './qc-live-bridge.css'
+import './qc-live-bridge'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
