7fd139547dc6e6b356df26146c3bf8885076571c
Hanjay6688 <jktr9n2g88@privaterelay.appleid.com>
2026-08-27 19:09:31 +0700

    style: load brand book readability layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './upgrade.css'
+import './brand-books.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
